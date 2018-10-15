52febdd6f4973f6ae388277cde66add70de1b31b
NaFFF345 <asjd581@gmail.com>
2018-10-16 00:11:54 +0900

    [add]ignorefile,script file,sass file

---
+++ script.js
@@ -0,0 +1,30 @@
+var start = new Date();
+
+var script = new Vue({
+    el: "#speakScript",
+    data: {
+        sentence: "賑やかすぎる 街の音",
+        sentence2: "あれこれ感情は スクランブル"
+    }
+});
+
+function changeTitle() {
+    var maaya = "Shiny Drive Moony Dive";
+    title.title = maaya;
+}
+
+var title = new Vue({
+    el: "#title",
+    data: {
+        title: "セツナ Ring a Bell"
+    }
+});
+
+function timer() {
+    nowTime = new Date();
+    if (nowTime - start >= 2000) {
+        changeTitle();
+    }
+}
+
+setInterval(timer, 1000);
